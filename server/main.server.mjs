af1979874dc9d71b935dca2e7cc0465e67eb8a1b
VitaliiKyrpach <126057381+VitaliiKyrpach@users.noreply.github.com>
2024-09-03 11:37:05 +0300

    Auto-generated commit

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-GJKCZ4LN.mjs";import"./chunk-7AMBXW7Y.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-BV57QG6N.mjs";import"./chunk-LVQCYECF.mjs";import"./chunk-HAOTC3BM.mjs";export{a as default};
